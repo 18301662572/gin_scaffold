49385a02fb14765ba1734e92bf9f9829940581c4
18301662572 <18301662572@163.com>
2020-02-24 22:16:07 +0800

    提交gin_grom

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/18301662572/gin_scaffold
+
+go 1.12
+
+require (
+	github.com/e421083458/gin_scaffold v0.0.0-20200217035828-8d38a3132f52
+	github.com/e421083458/golang_common v1.0.8
+	github.com/e421083458/gorm v1.0.1
+	github.com/garyburd/redigo v1.6.0
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607
+	github.com/gin-gonic/gin v1.5.0
+	github.com/go-playground/locales v0.13.0
+	github.com/go-playground/universal-translator v0.17.0
+	gopkg.in/go-playground/validator.v9 v9.31.0
+)
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/e421083458/gin_scaffold/public"
+	"github.com/e421083458/gin_scaffold/router"
+	"github.com/e421083458/golang_common/lib"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+//GORM: https://gorm.io/zh_CN/docs/index.html
+
+func main()  {
+	lib.InitModule("./conf/dev/",[]string{"base","mysql","redis",})
+	defer lib.Destroy()
+	public.InitMysql()
+	public.InitValidate()
+	router.HttpServerRun()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	router.HttpServerStop()
+}
